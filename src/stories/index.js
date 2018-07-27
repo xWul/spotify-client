2921870a9dc0f2e36f0faa10f1069cea1227c407
Wesley Moura <wesleytmoura@gmail.com>
2018-07-27 17:54:49 -0300

    Added storybook to project

---
+++ src/stories/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import { Button } from '@storybook/react/demo'
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
