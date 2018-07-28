48f343bfbfc5fc090bad86e3cb7120827f7a586b
Wesley Moura <wesleytmoura@gmail.com>
2018-07-28 14:36:22 -0300

    add global styled to SPA

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './screens/App'
 import registerServiceWorker from './registerServiceWorker'
+import globalStyle from './theme/globalStyle'
 import 'reset-css'
+
+globalStyle()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
