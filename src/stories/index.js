3ad65de30ad2f9e12effef1bceb39ce39a24f312
Wesley Moura <wesleytmoura@gmail.com>
2018-07-28 12:43:33 -0300

    updated storybook with Search and UI components

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import 'reset-css'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
+import globalStyle  from '../theme/globalStyle'
 
-import { Button } from '@storybook/react/demo'
+/* UI  */
+import Select from '../components/ui/Select'
+import InputText from '../components/ui/InputText'
+import Button from '../components/ui/Button'
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+/* COMPONENTS */
+import Options from '../components/Search/Options'
+import Search from '../components/Search'
+
+globalStyle()
+
+storiesOf('UI', module)
+  .add('Select', () => <Select><option>Option 1</option></Select>)
+  .add('Input Text', () => <InputText placeholder={'Type what you want'}/>)
+  .add('Button', () => <Button>Click</Button>)
+
+storiesOf('Search', module)
+  .add('Options', () => <Options onChange={action('changed')} />)
+  .add('Search Bar', () => <Search onSubmit={action('submited')} />)
+  
