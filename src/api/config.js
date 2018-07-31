775cbc564a906302c4e94217779a1493f0e0abc7
Wesley Moura <wesleytmoura@gmail.com>
2018-07-30 21:48:12 -0300

    added a token generation when api return 401

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,3 @@
 const API_URL = 'https://api.spotify.com/v1'
+export const TOKEN = window.localStorage.getItem('token')
 export default API_URL
--- src/screens/App.js
+++ src/screens/App.js
@@ -17,6 +17,23 @@ const Container = styled.main`
 `
 
 class App extends Component {
+  componentDidMount() {
+    const authURL = 'https://accounts.spotify.com/authorize?client_id=734facdc5d78467f87ff591c51e18345&response_type=token&redirect_uri=http://localhost:3000'
+    let hashParams = {};
+    let e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    // eslint-disable-next-line
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+
+    if (!hashParams.access_token) {
+      window.location.href = authURL;
+    } else {
+      window.localStorage.setItem('token', hashParams.access_token)
+    }
+  }
+
   render() {
     return (
       <Container>
