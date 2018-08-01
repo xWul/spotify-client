c83890e1d41d8adb069ba9cdef598ab8d5d807d8
Wesley Moura <wesleytmoura@gmail.com>
2018-07-31 23:31:12 -0300

    updated the ednpoint redirect of auth url

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,5 @@
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=734facdc5d78467f87ff591c51e18345&response_type=token&redirect_uri=https://stupefied-lamarr-f4af87.netlify.com'
+const { href } = window.location
+
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=734facdc5d78467f87ff591c51e18345&response_type=token&redirect_uri=${href}`
 
 export default AUTH_URL
