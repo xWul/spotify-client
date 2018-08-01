33fcd58ae93ea7bfbbaba234d09f9e912859b4a5
Wesley Moura <wesleytmoura@gmail.com>
2018-07-31 22:36:36 -0300

    changed auth endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,3 @@
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=734facdc5d78467f87ff591c51e18345&response_type=token&redirect_uri=http://localhost:3000'
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=734facdc5d78467f87ff591c51e18345&response_type=token&redirect_uri=https://stupefied-lamarr-f4af87.netlify.com'
 
 export default AUTH_URL
