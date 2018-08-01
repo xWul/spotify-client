7556125784dcc7e4c172bbce8c9ac797c7fda119
Wesley Moura <wesleytmoura@gmail.com>
2018-07-31 22:13:18 -0300

    added components storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,16 +6,19 @@ import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 import globalStyle  from '../theme/globalStyle'
 
+import { BrowserRouter as Router } from 'react-router-dom'
 /* UI  */
 import Select from '../components/ui/Select'
 import InputText from '../components/ui/InputText'
 import Button from '../components/ui/Button'
 import Alert from '../components/Alert'
 import Loader from '../components/Loader'
+import LikeButton from '../components/LikeButton'
 
 /* COMPONENTS */
 import Options from '../components/Search/Options'
 import Search from '../components/Search'
+
 
 globalStyle()
 
@@ -25,9 +28,9 @@ storiesOf('UI', module)
   .add('Button', () => <Button>Click</Button>)
   .add('Alert', () => <Alert>this is a error message</Alert>)
   .add('Loader', () => <Loader />)
-  
+  .add('LikeButton', () => <LikeButton />)
+  .add('LikeButton liked', () => <LikeButton isChecked={true}/>)
 
 storiesOf('Search', module)
   .add('Options', () => <Options onChange={action('changed')} />)
-  .add('Search Bar', () => <Search onSubmit={action('submited')} />)
-  
+  .add('Search Bar', () => <Router><Search onSubmit={action('submited')} /></Router>)  
